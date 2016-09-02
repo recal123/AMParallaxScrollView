c35e942f70db9cffd55481f1a72ec4c1bd0e9ca8
recal123 <191705008@qq.com>
2016-09-02 14:41:59 +0800

    建立项目文件
    建立项目文件

---
+++ AMParallaxScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AMParallaxScrollView
+//
+//  Created by Aimi on 16/9/2.
+//  Copyright © 2016年 aimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AMParallaxScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMParallaxScrollView
+//
+//  Created by Aimi on 16/9/2.
+//  Copyright © 2016年 aimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
